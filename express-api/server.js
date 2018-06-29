75d53c5b288c06f0007fd70b50181c3c2e0b046f
Rafael Barreto <rafaelgavabarreto@gmail.com>
2018-06-29 17:08:07 +0000

    database configuration

--- express-api/server.js
+++ express-api/server.js
@@ -2,13 +2,21 @@
 
 require('dotenv').config()
 
+const PORT = process.env.PORT || 4000
+
+const ENV         = process.env.ENV || "development";
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const express = require('express')
+const app         = express();
 
-const PORT = process.env.PORT || 4000
+// Configuration of Knex
+const knexConfig  = require("./knexfile");
+const knex        = require("knex")(knexConfig[ENV]);
+const knexLogger  = require('knex-logger');
 
-const app = express()
+// Log knex SQL queries to STDOUT as well
+app.use(knexLogger(knex));
 
 // Add Proper CORS headers to all routes
 app.use(cors())
@@ -16,8 +24,25 @@ app.use(cors())
 // Parses JSON Bodies in POST requests
 app.use(bodyParser.json())
 
+knex.select('*')
+  .from('users')
+  .then(function(rows) {
+    console.log(rows);
+  })
+  .catch(function(error) {
+    console.error(error)
+  });
+
 app.get('/', (req, res) => {
-  res.json({message: 'Hello World! from GET'})
+  res.json({message: 'Hello World! from GET'});
+  knex.select('*')
+  .from('users')
+  .then(function(rows) {
+    console.log(rows);
+  })
+  .catch(function(error) {
+    console.error(error)
+  });
 })
 
 app.get('/', (req, res) => {
