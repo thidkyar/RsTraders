18ed7424aed45421ea27d936c3cd6e10a69a7479
Taranvir Hidkyar <taranvirhidkyar@Taranvirs-MacBook-Pro.local>
2018-06-29 12:42:21 -0400

    ui changes to register form

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,22 +1,13 @@
 import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
 import { TextField } from '@material-ui/core';
+import "./Login.css";
 
 class Login extends Component {
   render() {
     return (
-      <div>
-        <div className = "App">
-      <form >
-         <h1> Login </h1>
-         <TextField label="Email" name="Email"/>
-         <TextField label="Password" name="Password"/>
-         <Button variant="contained" color="primary" >
-          Login
-        </Button>
-       </form>
-      </div>
-      </div>
+    <div>
+    </div>
     );
   }
 }
