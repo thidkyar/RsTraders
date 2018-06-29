ff6d468d88728a8974c9ae6fb02c87132ce68c94
Rafael Barreto <rafaelgavabarreto@gmail.com>
2018-06-29 16:03:25 +0000

    migrations files

---
+++ db/migrations/20180629155220_favorites.js
@@ -0,0 +1,18 @@
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema
+    .createTable('favorites', function(table) {
+      table.increments('id').primary();
+      table.string('users_id').references('id').inTable('users');
+      table.string('coin_id');
+      table.integer('rank');
+    })
+  ]);
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema
+    .dropTable('favorites')
+  ])
+};
