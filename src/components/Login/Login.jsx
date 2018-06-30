db43dbb219604cadc6c1c4c2e3d903dc55032791
Taranvir Hidkyar <taranvirhidkyar@Taranvirs-MacBook-Pro.local>
2018-06-30 15:06:26 -0400

    add basic login form

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -6,8 +6,23 @@ import "./Login.css";
 class Login extends Component {
   render() {
     return (
-    <div>
-    
+      <div className="session">
+      <h1>Sign in to RSTraders</h1>
+      <div className="register-form">
+        <form >
+          <TextField label="Email" name="email" />
+          <br />
+          <TextField label="Password" name="password" />
+          <br />
+          <Button
+            type="Submit"
+            variant="contained"
+            color="primary"
+          >
+            Login
+          </Button>
+        </form>
+      </div>
     </div>
     );
   }
