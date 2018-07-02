fb3e9b6a56c3bd2b0b04de7093798b56b14e0383
Taranvir Hidkyar <taranvirhidkyar@Taranvirs-MacBook-Pro.local>
2018-07-01 22:19:42 -0400

    added tooltip for chart and ui changes

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -3,6 +3,8 @@ import Button from "@material-ui/core/Button";
 import { Bar, Line, Pie, Area } from "react-chartjs-2";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
+import "./Chart.css";
+
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 
@@ -14,33 +16,33 @@ class Chart extends Component {
         labels: [],
         datasets: [
           {
-            data: [],
-          }, {
-
-          }
+            data: []
+          },
+          {}
         ]
       },
       options: {
-            scales: {
-              yAxes: [
-                {
-                  ticks: {
-                    beginAtZero: true
-                  }
-                }
-              ]         
+        legend: {
+          labels: {
+            fontColor: "orange"
+          }
+        },
+        title: {
+          display: true,
+          fontColor: "white",
+          text: "BitCoin"
         }
       }
     };
   }
-  
+
   componentDidMount() {
     this._setLabelDatafromAPI();
   }
 
   _setLabelDatafromAPI = () => {
     const coinCode = "BTC";
-    const url = `https://min-api.cryptocompare.com/data/histoday?fsym=${coinCode}&tsym=CAD&limit=10`;
+    const url = `https://min-api.cryptocompare.com/data/histoday?fsym=${coinCode}&tsym=CAD&limit=100`;
 
     fetch(url)
       .then(res => res.json())
@@ -57,7 +59,7 @@ class Chart extends Component {
           const highTime = x.high;
           const lowTime = x.low;
           //convert epoch time to readable time
-          const theTime = new Date(time*1000).toLocaleDateString()
+          const theTime = new Date(time * 1000).toLocaleDateString();
           //push items to set arrays
           timeData.push(theTime);
           costCloseData.push(closeTime);
@@ -76,7 +78,7 @@ class Chart extends Component {
               {
                 ...this.state.data.datasets,
                 data: costCloseData,
-                label: 'Close',
+                label: "Close",
                 borderColor: "#c45850",
                 lineTension: 0,
                 fill: false
@@ -84,30 +86,25 @@ class Chart extends Component {
               {
                 ...this.state.data.datasets,
                 data: costHighData,
-                label: 'High',
+                label: "High",
                 borderColor: "#3e95cd",
                 lineTension: 0,
                 fill: false
-              },{
+              },
+              {
                 ...this.state.data.datasets,
                 data: costLowData,
-                label: 'Low',
-                borderColor: "#3cba9f",
+                label: "Low",
+                borderColor: "#308D8C",
                 lineTension: 0,
                 fill: false
               }
             ]
-          },
-          options: {
-            title: {
-              display: true,
-              text: 'BitCoin'
-            }
-        }
+          }
         });
       });
   };
-  
+
   render() {
     const styles = theme => ({
       root: {
@@ -121,27 +118,70 @@ class Chart extends Component {
     });
     return (
       <div className="chart">
-      <Grid container spacing={0}>
-        <Grid item sm={2}>
-          <Paper > 
-          <h1>Bitcoin</h1>
-          <Button variant="outlined" color="primary"> Buy </Button>
-          <br/>
-          <Button variant="outlined" color="primary"> Sell</Button>
-          </Paper>
+        <Grid container spacing={0}>
+          <Grid item xs={10} sm={2}>
+            <Paper>
+              <h1>Bitcoin</h1>
+              <Button variant="contained" color="primary">
+                {" "}
+                Buy{" "}
+              </Button>
+              <br />
+              <Button variant="contained" color="primary">
+                {" "}
+                Sell
+              </Button>
+            </Paper>
+          </Grid>
+          <Grid item xs={10} sm={10}>
+            <Paper>
+              <Line
+                data={this.state.data}
+                width={100}
+                height={300}
+                options={{
+                  title: {
+                    display: true,
+                    fontColor: "white",
+                    text: "BITCOIN"
+                  },
+                  tooltips: {
+                    mode: "index",
+                    intersect: false
+                  },
+                  legend: {
+                    labels: {
+                      fontColor: "white"
+                    }
+                  },
+                  scales: {
+                    yAxes: [
+                      {
+                        gridLines: {
+                          display: true,
+                          color: '#707073'
+                        },
+                        position: "right",
+                        ticks: {
+                          fontColor: "white",
+                        }
+                      }
+                    ],
+                    xAxes: [
+                      {
+                        ticks: {
+                          fontColor: "white",
+                          maxTicksLimit: 8
+                        }
+                      }
+                    ]
+                  },
+                  maintainAspectRatio: false
+                }}
+              />
+            </Paper>
+          </Grid>
         </Grid>
-        <Grid item sm={10}>
-          <Paper ><Line
-          data={this.state.data}
-          width={100}
-          height={200}
-          options={{
-            maintainAspectRatio: false
-          }}
-        /></Paper>
-        </Grid>
-      </Grid>
-        
       </div>
     );
   }
