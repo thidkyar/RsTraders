4c3d0c4ca51cf0ac360222cdd9f985f4cc14dfe0
rafaelgavabarreto <rafaelgavabarreto@gmail.com>
2018-07-04 13:38:27 -0400

    Fix duplicate values in favorites

--- express-api/routes/favorites.js
+++ express-api/routes/favorites.js
@@ -24,18 +24,34 @@ module.exports = (knex) => {
 
   // Get information from login web page
   router.post("/favorites", (req, res) => {
-    console.log('HEY', req.session.user_id)
-      knex('favorites')
-        .insert([{
-          users_id: req.session.user_id,
-          coin_id: req.body.coin_id,
-          rank: req.body.rank
-        }])
-        .catch(function(error) {
-          console.error('Error: Inserting the favorites',error);
-          res.json({ sucess: false })
-        });
-      res.json({ sucess: true });
+    if (!req.session.user_id) {
+      res.json({
+        redirect: true,
+        url: '/'
+      })
+    } else {
+      knex
+        .select("*")
+        .from('favorites')
+        .where('users_id', req.session.user_id)
+        .where('coin_id', req.session.coin_id)
+        .then((results) => {
+          if (results.length != 0) {
+            knex('favorites')
+              .insert([{
+                users_id: req.session.user_id,
+                coin_id: req.body.coin_id,
+                rank: req.body.rank
+              }])
+              .catch(function(error) {
+                res.json({ sucess: false, message: error })
+              });
+              res.json({ sucess: true });
+          } else {
+            res.json({ sucess: false });
+          }
+      });
+    }
   });
 
   router.post("/favorites/delete/", (req, res) => {
