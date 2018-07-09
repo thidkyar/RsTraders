29a83794109fc5d694b2c1833a97578d53955907
rafaelgavabarreto <rafaelgavabarreto@gmail.com>
2018-07-09 13:28:30 -0400

    graphic in mainpage and color numbers

--- src/components/CryptoAPI/Crypto.jsx
+++ src/components/CryptoAPI/Crypto.jsx
@@ -28,6 +28,13 @@ const styles = theme => ({
   table: {
     minWidth: 100,
   },
+  maintablehead: {
+    textAlign: 'center'
+  },
+  maintable: {
+    padding: '4px 10px 4px 32px',
+    textAlign: 'left'
+  }
 });
 
 class Crypto extends Component {
@@ -57,18 +64,20 @@ class Crypto extends Component {
       .then(res => res.json())
       .then(result => {
         this.setState("Set state"[result]);
-        // console.log("Original Format", result);
+        console.log("Original Format", result);
         var coins = Object.values(result.data);
         this.setState({ coins: coins });
         // console.log("Here's the array", coins);
       });
   }
   render() {
+    const { classes } = this.props;
+    console.log('this.state.coins',this.state.coins)
     const { anchorEl } = this.state;
     return (
       <div className="Crypto-Ticker">
         <Grid container spacing={4}>
-          <Grid item xs={8} style={{ padding: '4%' }} >
+          <Grid item xs={8} style={{ padding: '2%' }} >
             <Paper settings={{ padding: '2%' }}>
 
               <h1 style={{ padding: '2em' }}> Top 100 Cryptocurrencies </h1>
@@ -76,26 +85,28 @@ class Crypto extends Component {
               <Table  >
                 <TableHead>
                   <TableRow>
-                    <TableCell width='10%'>Rank</TableCell>
-                    <TableCell width='15%'>Symbol</TableCell>
-                    <TableCell width='30%'>Name</TableCell>
-                    <TableCell width='30%'>Price</TableCell>
-                    <TableCell>Change (24h)</TableCell>
+                    <TableCell className={classes.maintablehead} width='1%'>Rank</TableCell>
+                    <TableCell className={classes.maintablehead} width='1%'>Symbol</TableCell>
+                    <TableCell className={classes.maintablehead} width='10%'>Name</TableCell>
+                    <TableCell className={classes.maintablehead} width='10%'>Price</TableCell>
+                    <TableCell className={classes.maintablehead} width='10%'>Change (24h)</TableCell>
+                    <TableCell className={classes.maintablehead} width='15%'>Price Graph (7d)</TableCell>
                   </TableRow>
-                </TableHead>
+                </TableHead> 
                 <TableBody>
                   {this.state.coins.map((coin, c) => {
                     return (
                       <TableRow key={c}>
-                        <TableCell>{coin.rank}</TableCell>
-                        <TableCell>{coin.symbol}</TableCell>
-                        <TableCell>{coin.name}</TableCell>
-                        <TableCell>
-                          $ {Math.round(coin.quotes.CAD.price * 100) / 100}
+                        <TableCell className={classes.maintable}>{coin.rank}</TableCell>
+                        <TableCell className={classes.maintable}>{coin.symbol}</TableCell>
+                        <TableCell className={classes.maintable}>{coin.name}</TableCell> 
+                        <TableCell className={classes.maintable}>{'$' + Math.round(coin.quotes.CAD.price * 100) / 100} </TableCell>
+                        <TableCell className={classes.maintable}>
+                        { (coin.quotes.CAD.percent_change_24h > 0) ? 
+                            <font color="#3c8229"> {coin.quotes.CAD.percent_change_24h + '%'}</font> :
+                            <font color="red"> {coin.quotes.CAD.percent_change_24h + '%'} </font> }
                         </TableCell>
-                        <TableCell>
-                          {coin.quotes.CAD.percent_change_24h} %
-                    </TableCell>
+                        <TableCell className={classes.maintable}><img class="sparkline" alt="sparkline" src={'https://s2.coinmarketcap.com/generated/sparklines/web/7d/usd/' + coin.id + '.png'}/></TableCell>
                       </TableRow>
                     );
                   })}
@@ -116,4 +127,4 @@ class Crypto extends Component {
   }
 }
 
-export default Crypto;
+export default withStyles(styles)(Crypto);
